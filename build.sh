a44a2934ee93617d0659c119045276f8bb70b356
Ming-Hsuan-Tu <qrnnis2623891@gmail.com>
2012-09-11 14:22:49 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #ln -s ~/vimrc2/.vimrc2 ~/.vimrc2
+mkdir -p ~/.vim_plugin2
 ln -s ~/vimrc2/.vim_plugin2/FuzzyFinder.vim ~/.vim_plugin2/FuzzyFinder.vim
 ln -s ~/vimrc2/.vim_plugin2/autocomplpop.vim ~/.vim_plugin2/autocomplpop.vim
 ln -s ~/vimrc2/.vim_plugin2/eclim.vim ~/.vim_plugin2/eclim.vim
