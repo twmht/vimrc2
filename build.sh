3bb1cb8912be66b010d9b9118c93f270623a58c0
mht <mht@ubuntu.ubuntu-domain>
2012-09-15 21:34:51 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-#ln -s ~/vimrc2/.vimrc2 ~/.vimrc2
+ln -s ~/vimrc2/.vimrc2 ~/.vimrc2
+mkdir -p ~/.vim2
+mkdir -p ~/.vim2/colors
+mkdir -p ~/.vim2/bundle
+git clone https://github.com/gmarik/vundle.git ~/.vim2/bundle/vundle
+cp ~/vimrc2/vundle.vim ~/.vim2/bundle/vundle/autoload/
+cp ~/vimrc/colors/wombat256.vim ~/.vim2/colors/
 mkdir -p ~/.vim_plugin2
 ln -s ~/vimrc2/.vim_plugin2/FuzzyFinder.vim ~/.vim_plugin2/FuzzyFinder.vim
 ln -s ~/vimrc2/.vim_plugin2/autocomplpop.vim ~/.vim_plugin2/autocomplpop.vim
 ln -s ~/vimrc2/.vim_plugin2/eclim.vim ~/.vim_plugin2/eclim.vim
 ln -s ~/vimrc2/.vim_plugin2/supertab.vim ~/.vim_plugin2/supertab.vim
+ln -s ~/vimrc2/.vim_plugin2/snipmate.vim ~/.vim_plugin2/snipmate.vim
